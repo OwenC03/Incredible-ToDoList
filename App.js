d80cd01089acc1b219accc62f23f066ff190fbad
OwenC03 <owen.carson@edu.sait.ca>
2024-10-28 16:30:48 -0600

    assignment Lab 3: Props and State Managment

--- App.js
+++ App.js
@@ -1,13 +1,20 @@
-import React from "react";
+import React, { useState} from "react";
 import { View, StyleSheet } from "react-native";
 import TodoForm from "./ToDoForm";
 import TodoList from "./ToDoList";
 
 export default function App() {
+
+  const [tasks] = useState([
+    "Do Laundry",
+    "Go to gym",
+    "Walk dog"
+  ]);
+
   return (
     <View style={styles.container}>
       <TodoForm />
-      <TodoList />
+      <TodoList tasks={tasks}/>
     </View>
   );
 }
